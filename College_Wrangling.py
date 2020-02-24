9008a5df18645999684747ceeb580e99481d4f15
jghuynh <jghuynh@dons.usfca.edu>
2020-02-23 17:02:25 -0800

    Coded a way to add column "Tier_Name" into data file

--- College_Wrangling.py
+++ College_Wrangling.py
@@ -13,5 +13,5 @@ myCollegeDF = pd.read_csv("C:\\Users\\jghuynh\\Documents\\Data_Visualization_360
 
 CAcolleges = myCollegeDF[myCollegeDF.state == "CA"]
 
-CAselectedCols = CAcolleges.loc[:, ["name", "type", "tier", "count", "k_median", "par_median", "female"]].dropna()
+CAselectedCols = CAcolleges.loc[:, ["name", "type", "tier", "tier_name", "count", "k_median", "par_median", "female"]].dropna()
 CAselectedCols.to_csv("CAselectedCols.csv", encoding='utf-8', index=False)
